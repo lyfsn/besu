031d2abe3939f1cde76bc92b167d51a38bbe3a85
lyfsn <dev.wangyu@proton.me>
2024-03-15 17:48:40 +0800

    up

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/GenesisState.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/GenesisState.java
@@ -204,7 +204,7 @@ public final class GenesisState {
       final List<GenesisAccount> genesisAccounts) {
     try (var worldState = createGenesisWorldState(dataStorageConfiguration)) {
 //      writeAccountsTo(worldState, genesisAccounts, null);
-      System.out.println("--debug--6.6.1 - " + worldState.rootHash());
+      System.out.println("--debug--6.6.1 - " + genesisAccounts.size() + "-- " + worldState.rootHash());
       return worldState.rootHash();
     } catch (Exception e) {
       throw new RuntimeException(e);
