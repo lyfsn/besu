eee16fca894a86bfb1f757a426f11f2df6e870eb
lyfsn <dev.wangyu@proton.me>
2024-03-19 15:52:43 +0800

    format

--- besu/src/main/java/org/hyperledger/besu/controller/BesuControllerBuilder.java
+++ besu/src/main/java/org/hyperledger/besu/controller/BesuControllerBuilder.java
@@ -115,7 +115,6 @@ import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.web3j.abi.datatypes.Bool;
 
 /** The Besu controller builder that builds Besu Controller. */
 public abstract class BesuControllerBuilder implements MiningParameterOverrides {
@@ -130,6 +129,7 @@ public abstract class BesuControllerBuilder implements MiningParameterOverrides 
           Optional.ofNullable(genesisConfig)
               .map(conf -> conf.getConfigOptions(genesisConfigOverrides))
               .orElseThrow();
+
   protected Boolean genesisFileCheckEnabled = Boolean.TRUE;
 
   /** The Sync config. */
