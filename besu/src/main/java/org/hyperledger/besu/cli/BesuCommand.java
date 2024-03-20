8a39c32ff4ea02b9e161c997a614062114a114de
lyfsn <dev.wangyu@proton.me>
2024-03-20 14:51:02 +0800

    debug

--- besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
+++ besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
@@ -2405,7 +2405,15 @@ public class BesuCommand implements DefaultCommandValues, Runnable {
   private String genesisConfig() {
     try {
       System.out.println("--debug--5.9---");
-      return Resources.toString(genesisFile.toURI().toURL(), UTF_8);
+
+      //record time
+        long startTime = System.currentTimeMillis();
+      String string = Resources.toString(genesisFile.toURI().toURL(), UTF_8);
+        //record time
+      long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime);
+        System.out.println("Time to read genesis file: " + duration + "ms");
+      return string;
     } catch (final IOException e) {
       throw new ParameterException(
           this.commandLine, String.format("Unable to load genesis URL %s.", genesisFile), e);
